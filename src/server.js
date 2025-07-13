e6c12d0a2f3811510cfd649695d92e254b5114ce
Mohamed <hrbim756@gmail.com>
2025-07-13 20:31:38 +0100

    Correction

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const produitRoutes = require('./routes/produits');
+const commandeRoutes = require('./routes/commandes');
+const livraisonRoutes = require('./routes/livraisons');
+const alerteStockRoutes = require('./routes/alertes_stock');
+const categorieRoutes = require('./routes/categories');
+const ligneCommandeRoutes = require('./routes/ligne_commande');
+const rapportRoutes = require('./routes/rapports');
 
 // Load environment variables
 dotenv.config();
@@ -16,9 +23,16 @@ connectDB();
 // Define routes
 app.get('/', (req, res) => res.send('Backend opérationnel'));
 app.use('/api/users', userRoutes);
+app.use('/api/produits', produitRoutes);
+app.use('/api/commandes', commandeRoutes);
+app.use('/api/livraisons', livraisonRoutes);
+app.use('/api/alertes-stock', alerteStockRoutes);
+app.use('/api/categories', categorieRoutes);
+app.use('/api/ligne-commande', ligneCommandeRoutes);
+app.use('/api/rapports', rapportRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000; // Fallback port if not defined
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
